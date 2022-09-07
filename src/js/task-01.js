ce8b8e1debe71d64021e80e5fc40fa5911513cbe
VladBodnar <104225683+VladBodnar@users.noreply.github.com>
2022-09-07 23:49:23 +0300

    111
    1

---
+++ src/js/task-01.js
@@ -0,0 +1,8 @@
+const listLiItemRef = document.querySelectorAll('.item');
+console.log('Number of categories:',listLiItemRef.length);
+
+const listLiCategoryRef = document.querySelectorAll('h2');
+console.log(...listLiCategoryRef);
+
+const listLiElementssRef = h2.querySelectorAll('li');
+console.log(listLiElementssRef);
